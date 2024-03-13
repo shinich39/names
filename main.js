89850bea15cbcc8081e2edccfea417bdcaacbad9
shinich39 <shinich39@gmail.com>
2024-03-14 03:12:39 +0900

    fix

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const createWindow = () => {
   mainWindow.loadFile('index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Set event listeners.
   mainWindow.webContents.on("did-finish-load", function() {
